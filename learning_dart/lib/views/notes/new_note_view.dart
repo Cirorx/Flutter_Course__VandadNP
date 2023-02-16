e85c33d44397f81b7047a70f1f2d63e7f4430500
Ciro <80367726+Cirorx@users.noreply.github.com>
2023-02-16 19:10:36 -0300

    Chapter 31 - Creating New Notes
    Main changes on the new_note_view by adding the view of the textfield, and adding the background operations for the textController to work successfully.

--- learning_dart/lib/views/notes/new_note_view.dart
+++ learning_dart/lib/views/notes/new_note_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:learning_dart/services/auth/auth_service.dart';
+import 'package:learning_dart/services/crud/notes_service.dart';
 
 class NewNoteView extends StatefulWidget {
   const NewNoteView({super.key});
@@ -8,13 +10,103 @@ class NewNoteView extends StatefulWidget {
 }
 
 class _NewNoteViewState extends State<NewNoteView> {
+  //We create here this note so that we are able to keep hold of the note
+  //on the screen when hot reloading
+  DatabaseNote? _note;
+  late final NotesService _notesService;
+
+  //We need a text editor controller for the note
+  late final TextEditingController _textController;
+
+  void _textControllerListener() async {
+    final note = _note;
+    if (note == null) {
+      return;
+    }
+    final text = _textController.text;
+    await _notesService.updateNote(
+      note: note,
+      text: text,
+    );
+  }
+
+  void _setupTextControllerListener() {
+    _textController.removeListener(_textControllerListener);
+    _textController.addListener(_textControllerListener);
+  }
+
+  @override
+  void initState() {
+    _notesService = NotesService();
+    _textController = TextEditingController();
+    super.initState();
+  }
+
+  Future<DatabaseNote> createNewNote() async {
+    //if note wasnt created, we create it
+    final existingNote = _note;
+    if (existingNote != null) return existingNote;
+
+    final currentUser = AuthService.firebase().currentUser!;
+    final email = currentUser.email!;
+    final owner = await _notesService.getUser(email: email);
+    //we create and return the new note
+    return await _notesService.createNote(owner: owner);
+  }
+
+  //If the user discards the note and go to the previous screen
+  //it is neccesary that the note isn't saved on the database
+  void _deleteNoteIfTextIsEmpty() {
+    final note = _note;
+    if (_textController.text.isEmpty && note != null) {
+      _notesService.deleteNote(id: note.id);
+    }
+  }
+
+  void _saveNoteIfTextNotEmpty() async {
+    //autosave the note
+    final note = _note;
+    final text = _textController.text;
+    if (_textController.text.isNotEmpty && note != null) {
+      await _notesService.updateNote(
+        note: note,
+        text: text,
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    _deleteNoteIfTextIsEmpty();
+    _saveNoteIfTextNotEmpty();
+    _textController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("New note"),
-      ),
-      body: const Text("Write your note here."),
-    );
+        appBar: AppBar(
+          title: const Text("New note"),
+        ),
+        body: FutureBuilder(
+          future: createNewNote(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.done:
+                _note = snapshot.data;
+                _setupTextControllerListener();
+                return TextField(
+                  controller: _textController,
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  decoration: const InputDecoration(
+                      hintText: "Start typing your notes here..."),
+                );
+              default:
+                return const CircularProgressIndicator();
+            }
+          },
+        ));
   }
 }
